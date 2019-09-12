53fd8a1a208b09c5c412293639d28fd14f3e747c
Leonardo Diaz <leonardo.diaz.magallanes@gmail.com>
2019-09-12 22:06:01 +0200

    Fix error que ejecutaba mvn antes de escribir en el app.constants. Add: Git limpia los archivos modificados. Se capturan errores y se mejora los logs

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 
 /**
  * By 50characters
- * 
  */
 const fs = require("fs");
 const xpath = require('xpath');
 const dom = require('xmldom').DOMParser;
 const readlineSync = require('readline-sync');
-const spawn = require('child_process').spawn;
+const spawnSync = require('child_process').spawnSync;
 const path = require('path');
 const find = require('find');
 
@@ -21,11 +20,15 @@ const BUILD_TIMESTAMP = new Date().getTime();
 
 init();
 async function init() {
-    console.info(`Angular${ANGULAR_JS ? 'JS' : ''} detectado.`);
+    console.time("[EXIT] Build war");
+    checkFiles();
+    console.info(`[INFO] Angular${ANGULAR_JS ? 'JS' : ''} detectado.`);
     const profile = readlineSync.keyInYN(`Es war de produccion?`, {defaultInput: 'Y'}) ? 'prod' : 'dev';
     const skipTest = !readlineSync.keyInYN(`Ejecutar TEST?`, {defaultInput: 'Y'});
 
-    const buildVersion = await editPom(skipTest);
+    let pomXmlDom = await readPom();
+    const buildVersion = await editPomVersion(pomXmlDom, null);
+    if (skipTest) await editPomSkipTest(pomXmlDom);
     if (ANGULAR_JS) await editAppConstants(buildVersion);
     if ('dev' === profile) {
         if (ANGULAR_JS && readlineSync.keyInYN(`Quieres quitar cache en index.html?`, {defaultInput: 'Y'})) {
@@ -33,7 +36,20 @@ async function init() {
         }
     }
     await execMaven(profile);
-    cleanBuild(profile);
+    await cleanBuild(profile);
+    //Volvemos a agregar la build version al node despues de descartar todo
+    pomXmlDom = await readPom();
+    await editPomVersion(pomXmlDom, buildVersion);
+    console.info('[INFO] War generado correctamente! :)');
+    console.timeEnd("[EXIT] Build war");
+}
+
+function checkFiles() {
+    const pomFile = fs.existsSync(POM_FILE);
+    if (!pomFile) {
+        printError("No se encuentra el pom.xml");
+        process.exit();
+    }
 }
 
 /**
@@ -44,55 +60,65 @@ function detectAngularJS() {
     return !hasAngularJson;
 }
 
+function readPom() {
+    return new Promise((resolve, reject) => {
+        try {
+            fs.readFile(POM_FILE, "utf-8", function (err, xml) {
+                if (err && printError(err)) reject(null);
+                resolve(new dom().parseFromString(xml));
+            });
+        } catch(err) {reject(null);}
+    });
+}
+
 /**
  * Solicitamos version y
  * Editamos el pom.xml
  */
-function editPom(skipTest) {
+function editPomVersion(pomXmlDom, projectVersion) {
     return new Promise((resolve, reject) => {
         try {
-            fs.readFile(POM_FILE, "utf-8", function (err, xml) {
-                if (err && console.error(err)) reject();
-                var doc = new dom().parseFromString(xml);
-                var versionTags = xpath.select("//*[local-name(.)='version']", doc);
-                var versionElement = null;
-                for (var i = 0; i < versionTags.length; i++) {
-                    if (versionTags[i].parentNode && versionTags[i].parentNode.tagName === 'project') {
-                        versionElement = versionTags[i];
-                        break;
-                    }
+            let versionTags = xpath.select("//*[local-name(.)='version']", pomXmlDom);
+            let versionElement = null;
+            for (let i = 0; i < versionTags.length; i++) {
+                if (versionTags[i].parentNode && versionTags[i].parentNode.tagName === 'project') {
+                    versionElement = versionTags[i];
+                    break;
                 }
-                if (null !== versionElement) {
-                    let buildVersion = versionElement.textContent;
+            }
+            if (null !== versionElement) {
+                let buildVersion = projectVersion || versionElement.textContent;
+                if (!projectVersion) {
                     const inputVersion = readlineSync.question(`Introduzca NUEVA VERSION: (${buildVersion}) `, {
                         defaultInput: buildVersion
                     });
-                    
+                        
                     if (inputVersion && inputVersion.length !== 0) buildVersion = inputVersion;
-            
-                    let newNode = new dom().parseFromString(`<version>${buildVersion}</version>`);
-                    versionElement.parentNode.replaceChild(newNode, versionElement);
-
-                    if (skipTest) {
-                        const tagSkipTest = new dom().parseFromString(`<maven.test.skip>true</maven.test.skip>`);
-                        var propertiesTags = xpath.select("//*[local-name(.)='properties']", doc);
-                        var propertiesElement = null;
-                        for (var i = 0; i < propertiesTags.length; i++) {
-                            if (propertiesTags[i].parentNode && propertiesTags[i].parentNode.tagName === 'project') {
-                                propertiesElement = propertiesTags[i];
-                                break;
-                            }
-                        }
-                        propertiesElement.appendChild(tagSkipTest);
-                    }
-            
-                    writeFile(POM_FILE, doc.toString());
-                    resolve(buildVersion);
                 }
-            });
-        } catch(err) {
-            reject();
-        }
+            
+                versionElement.textContent = buildVersion;
+                writeFile(POM_FILE, pomXmlDom.toString()).then(() => resolve(buildVersion)).catch(reject);
+                
+            }
+        } catch(err) {reject();}
+    });
+}
+
+function editPomSkipTest(pomXmlDom) {
+    return new Promise((resolve, reject) => {
+        try {
+            const tagSkipTest = new dom().parseFromString(`<maven.test.skip>true</maven.test.skip>`);
+            let propertiesTags = xpath.select("//*[local-name(.)='properties']", pomXmlDom);
+            let propertiesElement = null;
+            for (let i = 0; i < propertiesTags.length; i++) {
+                if (propertiesTags[i].parentNode && propertiesTags[i].parentNode.tagName === 'project') {
+                    propertiesElement = propertiesTags[i];
+                    break;
+                }
+            }
+            propertiesElement.appendChild(tagSkipTest);
+            writeFile(POM_FILE, pomXmlDom.toString()).then(resolve).catch(reject);
+        } catch(err) {reject();}
     });
 }
 
@@ -100,11 +126,11 @@ function editPom(skipTest) {
  * Quitamos cache a los ficheros en el index.html cuando modo dev
  */
 function editIndexHtml() {
-    console.info("Editando index.html");
+    console.info("[INFO] Editando index.html");
     return new Promise((resolve, reject) => {
         try {
             fs.readFile(path.join(WEBAPP_DIR,'index.html'), "utf-8", function (err, html) {
-                if (err && console.error(err)) reject();
+                if (err && printError(err)) reject();
                 let removeCache = false;
                 do {
                     const inputStr = readlineSync.question(`archivos, ej: oferta: `);
@@ -112,11 +138,10 @@ function editIndexHtml() {
             
                     removeCache = readlineSync.keyInYN(`Quieres continuar quitando cache?`, {defaultInput: 'Y'});
                 } while (removeCache);
-                writeFile(path.join(WEBAPP_DIR,'index.html'), html);
-                resolve();
+                writeFile(path.join(WEBAPP_DIR,'index.html'), html).then(resolve).catch(reject);
             });
         } catch (err) {
-            console.error(err);
+            printError(err);
             reject();
         }
     });
@@ -134,13 +159,12 @@ function editAppConstants(buildVersion) {
             if (!CONSTANTS_FILE) return resolve();
 
             fs.readFile(CONSTANTS_FILE, "utf-8", function (err, data) {
-                if (err) console.error(err);
+                if (err) printError(err);
                 const expVersion = new RegExp("\(\'VERSION\',.*\)");
                 const expTimestamp = new RegExp("\(\'BUILD_TIMESTAMP\',.*\)");
                 data = data.replace(expVersion, `'VERSION', '${buildVersion}')`);
                 data = data.replace(expTimestamp, `'BUILD_TIMESTAMP', ${BUILD_TIMESTAMP})`);
-                writeFile(CONSTANTS_FILE, data);
-                resolve();
+                writeFile(CONSTANTS_FILE, data).then(resolve).catch(reject);
             });
         } catch (err) {
             reject(err);
@@ -152,9 +176,9 @@ function editAppConstants(buildVersion) {
  * Solo AngularJS
  */
 function findConstantsFile() {
-    console.time('Find file constants');
+    console.time('[INFO] Find file constants');
     var findConstants = find.fileSync('app.constants.js', WEBAPP_DIR);
-    console.timeEnd('Find file constants');
+    console.timeEnd('[INFO] Find file constants');
     return ((findConstants || []).length > 0) ? findConstants[0] : null;
 }
 
@@ -166,35 +190,57 @@ function findConstantsFile() {
  *  - Angular: mvn package -Pdev,webpack
  * @param {string} profile 
  */
-async function execMaven(profile) {
-    profile = profile || 'dev';
-    if ('dev' == profile && !ANGULAR_JS) profile += ',webpack';
-    await execute(`mvn clean package -P${profile}`);
+function execMaven(profile) {
+    return new Promise((resolve, reject) => {
+        try {
+            //TODO: comprobar que exista mvn en el path process.env.PATH
+            //si no existe, comprobar en el pwd si existe el wrapper ./mvnw
+            let mvnCommand = 'mvn';
+            profile = profile || 'dev';
+            if ('dev' == profile && !ANGULAR_JS) profile += ',webpack';
+            execute(`${mvnCommand} clean package -P${profile}`)
+                .then(resolve).catch(reject);
+        } catch(err) {reject();}
+    });
 }
 
 function writeFile(file, data) {
-    try {
-        console.info(`Writing file: ${file}`);
-        fs.writeFile(file, data, function (err, res) {
-            if (err) console.error(err);
-        });
-    } catch (error) {
-        console.error(error);
-    }
+    return new Promise((resolve, reject) => {
+        try {
+            console.info(`[INFO] Writing file: ${file}`);
+            fs.writeFile(file, data, function (err, res) {
+                if (err) {
+                    printError(err);
+                    reject(err);
+                }
+                resolve(res);
+            });
+        } catch (error) {
+            printError(error);
+            reject(error);
+        }
+    });
 }
 
-function cleanBuild() {
-    //execute(`git checkout -- ${path.join(WEBAPP_DIR,'index.html')}`);
+async function cleanBuild(profile) {
+    //Clean
+    await execute(`git checkout -- ${path.join(WEBAPP_DIR,'index.html')}`);
+    await execute(`git checkout -- ${POM_FILE}`);
 }
 
 function execute(command) {
-    console.info(command);
+    console.info('[INFO] ' + command);
     return new Promise((resolve, reject) => {
         try {
-            spawn(command, { stdio: 'inherit', shell: true });
+            spawnSync(command, { stdio: 'inherit', shell: true })
             resolve();
         } catch (err) {
             reject(err);
         }
     });
 }
+
+function printError(err) {
+    console.error(`[ERROR] ${err}`);
+}
+
